66181ee301dd1ae0366ed1964bb3d14ffb534c3c
ssuukko <tlstpstpdl11@gmail.com>
2025-01-14 21:50:02 +0900

    feat: 비영속

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,36 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJpa");
+
+            // 영속
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
